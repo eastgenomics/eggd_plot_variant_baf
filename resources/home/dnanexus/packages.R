73aa674239203947d8499a4cb66144228a703fd2
pofuoku <princeofuoku@gmail.com>
2025-01-23 12:23:08 +0000

    initial commit

---
+++ resources/home/dnanexus/packages.R
@@ -0,0 +1,20 @@
+libPath <- "~/R/library"
+
+if (!dir.exists(libPath)) {
+  dir.create(libPath, recursive = TRUE)
+}
+
+.libPaths(libPath)
+#options(repos = c(CRAN = "https://cloud.r-project.org"))
+install.packages(c("stringr", "dplyr"), lib = libPath)
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager", lib = libPath)
+}
+BiocManager::install(c("karyoploteR", "biomaRt"), lib = libPath)
+
+install.packages(
+  "https://github.com/pola-rs/r-polars/releases/latest/download/polars__x86_64-pc-linux-gnu.gz",
+  repos = NULL,
+  lib = libPath
+)
