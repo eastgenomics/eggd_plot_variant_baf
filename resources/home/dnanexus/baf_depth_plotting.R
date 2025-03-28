034027e64749884ae7b45569fc63d70241c9dc8a
ChrisPyatt <christopher.pyatt1@nhs.net>
2025-03-28 14:11:56 +0000

    add argparse

--- resources/home/dnanexus/baf_depth_plotting.R
+++ resources/home/dnanexus/baf_depth_plotting.R
@@ -32,6 +32,34 @@ library(stringr, quietly = TRUE)
 library(karyoploteR, quietly = TRUE)
 library(dplyr, quietly = TRUE)
 library(polars, quietly = TRUE)
+library(argparse, quietly = TRUE)
+
+
+# create parser object
+parser <- ArgumentParser()
+
+# specify our desired options 
+# by default ArgumentParser will add an help option 
+parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
+    help="Print extra output [default]")
+parser$add_argument("-q", "--quietly", action="store_false", 
+    dest="verbose", help="Print little output")
+parser$add_argument("-c", "--count", type="integer", default=5, 
+    help="Number of random normals to generate [default %(default)s]",
+    metavar="number")
+parser$add_argument("--generator", default="rnorm", 
+    help = "Function to generate random deviates [default \"%(default)s\"]")
+parser$add_argument("--mean", default=0, type="double",
+    help="Mean if generator == \"rnorm\" [default %(default)s]")
+parser$add_argument("--sd", default=1, type="double",
+        metavar="standard deviation",
+    help="Standard deviation if generator == \"rnorm\" [default %(default)s]")
+                                        
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults, 
+args <- parser$parse_args()
+
+
 
 # List of functions
 ##################################
